fbd6931cb1db755d3583b1f290a3d233d8630266
richardmwibowo <63839269+RichardWibowo@users.noreply.github.com>
2022-10-09 19:48:14 +1100

    set up a input field

--- my-app/src/Pages/search.jsx
+++ my-app/src/Pages/search.jsx
@@ -1,25 +1,36 @@
 import React, {useState} from 'react'
 
 const Search = () =>{
-    const [searchInput, setSearchInput] = useState("");
-    let inputHandler = (e) => {
-        //convert input text to lower case
-        var lowerCase = e.target.value.toLowerCase();
-        setInputText(lowerCase);
+    const [value, setValue] = useState("");
+
+    const onChange = (event) => {
+        setValue(event.target.value);
+    };
+
+    const onSearch = (searchTerm) => {
+        setValue(searchTerm);
+        // our api to fetch the search result
+        console.log("search ", searchTerm);
       };
     return ( 
-    <div className='bg-gradient-to-r from-green-400 flex justify-center items-center'>   
-        <h1 className= "md:text-5xl sm:text-4xl text-3xl font-bold m:py-6">
-            find your super
-        </h1>
-        <div>
-        <input
-            type="text"
-            placeholder="Search here"
-            onChange={inputHandler}
-            value={searchInput} />
+    <div className='bg-gradient-to-r from-green-400 '>   
+        <div className='flex justify-center items-center'>
+            <h1 className= "md:text-5xl sm:text-4xl text-3xl font-bold m:py-6">
+                find your super
+            </h1>
         </div>
+        <div className='flex flex-col sm:flex-row items-center justify-between w-full'>
+            <input
+              className='p-3 flex w-full rounded-md text-black'
+              type='super'
+              placeholder='find your super now'
+            />
+            <button className='bg-[#00df9a] text-black rounded-md font-medium w-[200px] ml-4 my-6 px-6 py-3'>
+              Find now
+            </button>
+          </div>
     </div>
+    
     )
 }
 
